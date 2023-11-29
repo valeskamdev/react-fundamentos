34f5027060d3fe444b1ce80fb85609120c41c83e
Valeska Marques <valeskkitty@gmail.com>
2023-11-29 10:33:19 -0300

    refactor: reset geral

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+/*
+  Importação do primeiro/principal componente, arquivo em que carregamos
+  os elementos da página (como se fosse um index/home)
+ */
+import App from './App.jsx'
+
+// Inicialização dos recursos do React, manipulando a div#root existente na index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
