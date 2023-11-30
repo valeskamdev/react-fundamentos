abdb3d9a2555d401e0e71d8a85d3127b5b170302
Valeska Marques <valeskkitty@gmail.com>
2023-11-30 09:19:33 -0300

    feat: componente artigo

---
+++ src/components/Artigo.jsx
@@ -0,0 +1,10 @@
+function Artigo() {
+  return(
+    <article>
+      <h3>Artigo 1</h3>
+      <p>Conteúdo do artigo 1...</p>
+    </article>
+  )
+}
+
+export default Artigo;
--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,3 +1,5 @@
+import Artigo from './Artigo.jsx';
+
 function Conteudo() {
   return(
     <main>
@@ -9,20 +11,10 @@ function Conteudo() {
           saepe sed voluptatum.
           Commodi cumque eaque earum facere itaque nihil porro?</p>
 
-        <article>
-          <h3>Artigo 1</h3>
-          <p>Conteúdo do artigo 1...</p>
-        </article>
-
-        <article>
-          <h3>Artigo 2</h3>
-          <p>Conteúdo do artigo 2...</p>
-        </article>
-
-        <article>
-          <h3>Artigo 3</h3>
-          <p>Conteúdo do artigo 3...</p>
-        </article>
+        {/* Reaproveitamento de Componentes */}
+        <Artigo />
+        <Artigo />
+        <Artigo />
 
       </section>
     </main>
