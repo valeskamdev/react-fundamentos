a0cd440492bef486539e51e4951e8e486fcfbd12
Valeska Marques <valeskkitty@gmail.com>
2023-12-01 09:47:31 -0300

    refactor: seletor & para hover

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,15 +5,23 @@ const StyledMenu = styled.nav`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  
+
   a {
     color: white;
     text-decoration: none;
-  }
-  
-  a:hover {
-    background-color: deeppink;
-    color: black;
+    width: 30%;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: transform 100ms;
+    
+    // & -> referência ao seletor pai (a)
+    &:hover {
+      background-color: pink;
+      color: black;
+      transform: scale(1.05);
+    }
   }
 
 `;
