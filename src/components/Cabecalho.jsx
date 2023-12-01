17eb5697643ce3628fe52e53c92614150174b517
Valeska Marques <valeskkitty@gmail.com>
2023-12-01 09:29:15 -0300

    style: aplicação do styled components no header

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,12 +1,31 @@
 import Menu from './Menu.jsx';
 
+// Importando lib Styled Components
+import styled from 'styled-components';
+
+const StyledCabecalho = styled.header`
+  background-color: black;
+  color: white;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+  
+  hr {
+    width: 50%;
+    background-color: pink;
+    border: none;
+    height: 4px;
+    margin: 8px auto;
+  }
+`;
+
 function Cabecalho() {
   return(
-    <header>
+    <StyledCabecalho>
       <h1>Olá React </h1>
       <hr/>
       <Menu />
-    </header>
+    </StyledCabecalho>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-/*
-  Importação do primeiro/principal componente, arquivo em que carregamos
-  os elementos da página (como se fosse um index/home)
- */
 import App from './App.jsx'
+import './index.css';
 
-// Inicialização dos recursos do React, manipulando a div#root existente na index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
