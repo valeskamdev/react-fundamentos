0f8e4fecd8f345536016a0aaeb4817332c9e97a9
Valeska Marques <valeskkitty@gmail.com>
2023-12-01 09:42:33 -0300

    style: aplicação do styled components no nav

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,29 @@
+import styled from 'styled-components';
+
+const StyledMenu = styled.nav`
+  height: 50px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  
+  a:hover {
+    background-color: deeppink;
+    color: black;
+  }
+
+`;
 function Menu() {
   return(
-    <nav>
+    <StyledMenu>
       <a href="">Home</a>
       <a href="">Produtos</a>
       <a href="">Contato</a>
-    </nav>
+    </StyledMenu>
   )
 }
 
