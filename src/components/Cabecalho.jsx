116db6387819e06cab52c31dcd3dd5ffcdd707f7
Valeska Marques <valeskkitty@gmail.com>
2023-12-06 10:31:58 -0300

    refactor: importando estilização cabecalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,23 +1,5 @@
 import Menu from './Menu.jsx';
-
-// Importando lib Styled Components
-import styled from 'styled-components';
-
-const StyledCabecalho = styled.header`
-  background-color: black;
-  color: white;
-  font-size: 1.2rem;
-  font-weight: bold;
-  text-align: center;
-  
-  hr {
-    width: 50%;
-    background-color: pink;
-    border: none;
-    height: 4px;
-    margin: 8px auto;
-  }
-`;
+import { StyledCabecalho } from '../styles/styles.js';
 
 function Cabecalho() {
   return(
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
+const StyledCabecalho = styled.header`
+  background-color: black;
+  color: white;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+  
+  hr {
+    width: 50%;
+    background-color: pink;
+    border: none;
+    height: 4px;
+    margin: 8px auto;
+  }
+`;
+
 const StyledRodape = styled.footer`
   background-image: linear-gradient(to right top, #919ff6, #b6a9f7, #d3b5f7, #eac3f8, #fdd2fa);
   height: 30vh;
@@ -93,4 +109,4 @@ const StyledSection = styled.section`
   }
 `;
 
-export { StyledRodape, StyledMenu, StyledConteudo, StyledArtigo, StyledSection }
+export { StyledRodape, StyledMenu, StyledConteudo, StyledArtigo, StyledSection, StyledCabecalho }
