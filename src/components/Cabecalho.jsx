6f1c9c4c3258eaf93b5a46a65431ba68d1ff595b
Valeska Marques <valeskkitty@gmail.com>
2023-12-06 08:53:57 -0300

    feat: evento com função inline

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -4,7 +4,11 @@ import { StyledCabecalho } from '../styles/styles.js';
 function Cabecalho() {
   return(
     <StyledCabecalho>
-      <h1>Olá React </h1>
+      {/*
+        Propriedade de evento que executará, após o clique, uma função intermediária
+        que fará ações (chamar outras funções, executar outros processos etc).
+      */}
+      <h1 onClick={ () => alert("Exemplo 1") }>Olá React </h1>
       <hr/>
       <Menu />
     </StyledCabecalho>
