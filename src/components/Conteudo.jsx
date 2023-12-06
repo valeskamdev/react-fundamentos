ff2f2365e226cf8464766e02830de72f5864c547
Valeska Marques <valeskkitty@gmail.com>
2023-12-06 09:01:28 -0300

    feat: evento com chamada de função

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -4,10 +4,22 @@ import cursos from '../../modules/cursos.js';
 
 function Conteudo() {
   const dataLancamento = ["02/07/2005", "21/02/2011", "30/11/2014"];
+
+  /*
+    Função que será chamada a partir do evento
+    acionado por um componente/elemento.
+   */
+  const exemplo2 = () => {
+    alert("Exemplo2");
+  }
+
   return(
     <StyledConteudo>
       <section>
         <h2>Conteúdo da aplicação</h2>
+
+        <button onClick={exemplo2}>Exemplo 2</button>
+
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
           cumque, cupiditate
           explicabo facilis id laboriosam molestias mollitia pariatur quasi
