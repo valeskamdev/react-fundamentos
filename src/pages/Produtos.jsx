67025746b0ca610cfdf6366de82bf73e80f9e29c
Valeska Marques <valeska.amarques@senacsp.edu.br>
2024-01-15 11:12:25 -0300

    feat: state de produtos

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,6 +1,16 @@
+import {useState} from "react";
+
 function Produtos() {
 
   const carregarDados = async () => {
+
+    /*
+      O state "produtos" é iniciado como um array vazio. Posteriormente
+      (após o carregamento dos dados da API), ele será preechido com os objetos/produtos.
+     */
+
+    const [produtos, setProdutos] = useState([]);
+    
     try {
       const resposta = await fetch(`https://fakestoreapi.com/products`);
       const dados = await resposta.json();
