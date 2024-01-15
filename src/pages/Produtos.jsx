cb9195dc4a84e05f38fe9cda2a169dc5003100c6
VALESKA AFFONSO MARQUES <valeska.amarques@senacsp.edu.br>
2024-01-15 10:53:48 -0300

    feat: uso do fetch com API fake

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,4 +1,18 @@
 function Produtos() {
+
+  const carregarDados = async () => {
+    try {
+      const resposta = await fetch(`https://fakestoreapi.com/products`);
+      const dados = await resposta.json();
+      console.log(dados);
+    } catch (e) {
+      console.error('Houve um erro: ' + e);
+
+    }
+  }
+
+  carregarDados();
+
   return <h2>Produtos</h2>
 }
 
