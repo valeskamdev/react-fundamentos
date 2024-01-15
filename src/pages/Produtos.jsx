400a5ae7a3503f1c89a6459e1ce618934936ff2e
Valeska Marques <valeska.amarques@senacsp.edu.br>
2024-01-15 11:29:20 -0300

    useEffect para carregamento da API

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,29 +1,43 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function Produtos() {
 
-  const carregarDados = async () => {
+  const [produtos, setProdutos] = useState([]);
 
-    /*
-      O state "produtos" é iniciado como um array vazio. Posteriormente
-      (após o carregamento dos dados da API), ele será preechido com os objetos/produtos.
-     */
+  /*
+    Fluxo geral de funcionamento do código abaixo:
 
-    const [produtos, setProdutos] = useState([]);
-    
-    try {
-      const resposta = await fetch(`https://fakestoreapi.com/products`);
-      const dados = await resposta.json();
-      console.log(dados);
 
-      setProdutos(dados);
-    } catch (e) {
-      console.error('Houve um erro: ' + e);
+    1) useEffect é carregado UMA VEZ e APÓS a montagem do page (Produtos)
+    Obs.: o [] indica que o useEffect não tem dependência adicionais e que será executados somente UMA VEZ.
 
+    2) Em seguida, ele executa a função 'carregarDados'
+    3) Ao término dela, atualiza o state (produtos)
+   */
+
+    // Gerenciando o efeito colateral,do componente para o carregamento dos dados da API.
+
+  useEffect(() => {
+    const carregarDados = async () => {
+
+      /*
+        O state "produtos" é iniciado como um array vazio. Posteriormente
+        (após o carregamento dos dados da API), ele será preechido com os objetos/produtos.
+       */
+
+      try {
+        const resposta = await fetch(`https://fakestoreapi.com/products`);
+        const dados = await resposta.json();
+        console.log(dados);
+
+        setProdutos(dados);
+      } catch (e) {
+        console.error('Houve um erro: ' + e);
+
+      }
     }
-  }
-
-  carregarDados();
+    carregarDados();
+  }, []);
 
   return <h2>Produtos</h2>
 }
